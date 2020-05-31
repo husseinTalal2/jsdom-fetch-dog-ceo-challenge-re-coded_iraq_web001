fa24d5bec615ee12556098804e149a9a85bfd90b
Hussein Talal <hossientalal2@gmail.com>
2020-05-31 22:00:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 console.log('%c HI', 'color: firebrick');
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
 document.addEventListener('DOMContentLoaded',()=>{
   fetch(imgUrl)
   .then(resp => resp.json())
   .then(resp => addImgElems(resp));
 })
+
+document.addEventListener('DOMContentLoaded',()=>{
+  fetch(breedUrl)
+  .then(resp => resp.json())
+  .then(resp => console.log(resp));
+})
+
+function addBreeds(){
+  
+}
+
 function addImgElems(obj){
   const imgsContainer = document.getElementById('dog-image-container');
   imgsArray = obj.message;
   const img = document.createElement('img');
   for(elem of imgsArray){
-    console.log(elem)
     const img = document.createElement('img');
     imgsContainer.appendChild(img);
     img.setAttribute('src',elem);
   }
-  //console.log(imgsArray);
 }
