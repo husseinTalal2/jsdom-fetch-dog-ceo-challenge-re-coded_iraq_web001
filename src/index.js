2cab420067348535c5c8640e5bbbfbd87b0b4649
Hussein Talal <hossientalal2@gmail.com>
2020-05-31 21:26:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 console.log('%c HI', 'color: firebrick')
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 document.addEventListener('DOMContentLoaded',()=>{
-  console.log("its loaded");
+  Fetch(imgUrl)
+  .then(response => {
+    response.json();
+  })
+  .then(data => console.log(data));
 })
